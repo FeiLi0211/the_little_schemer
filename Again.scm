ec065dee52ee27aea786e08f41c7e868d6a5fb3c
Fei Li <feili0211@gmail.com>
2016-07-31 11:37:34 +0800

    (2. Do It, Do It Again, and Again, and Again ...)

---
+++ Again.scm
@@ -0,0 +1,39 @@
+;;;; 2. Do It, Do It Again, and Again, and Again ...
+
+(define lat?
+  (lambda (l)
+    (if (null? l)
+        #t
+        (and (atom? (car l))
+             (lat? (cdr l))))))
+
+(lat? '(Jack Sprat could eat no chicken fat))
+
+(lat? '((Jack) Sprat could eat no chicken fat))
+
+(lat? '())
+
+(lat? '(bacon and eggs))
+
+(lat? '(bacon (and eggs)))
+
+(or (null? '()) (atom? '(d e f g)))
+
+(or (null? '(a b c)) (null? '()))
+
+(or (null? '(a b c)) (null? '(atom)))
+
+(define member?
+  (lambda (a lat)
+    (if (null? lat)
+        #f
+        (or (eq? (car lat) a)
+            (member? a (cdr lat))))))
+
+(member? 'tea '(coffee tea or milk))
+
+(member? 'poached '(fried eggs and scrambled eggs))
+
+(member? 'meat '(mashed potatoes and meat gravy))
+
+(member? 'liver '(bagels and lox))
